f5680e1d804cd5264bcc9b6106f970fa7e55102f
Kauã Santos <kaua.hs.santos0100@gmailcom>
2025-01-14 17:33:09 -0300

    Add print(user.length);

---
+++ aulas/aula002/main.dart
@@ -0,0 +1,35 @@
+void main(List<String> args) {
+  print("Variáveis em Dart");
+
+  print("Tipo Int");
+
+  int n1 = 1;
+  print(n1);
+  print("Para números Inteiros");
+
+  print("");
+
+  print("Tipo Double");
+
+  double d1 = 1.5;
+  print(d1);
+  print("Para números Quebrados");
+
+  print("");
+
+  print("TIpo Bool");
+
+  bool b1 = true;
+  print(b1);
+  print("Para Verdadiro ou Falso");
+
+  print("");
+
+  print("Tipo String");
+
+  String nomeOuFrases = "Esse é uma frase escrita dentro da variável String";
+  print(nomeOuFrases);
+
+  print("Para palavras, frases ou apenas letras.");
+
+}
---
+++ aulas/aula004/main.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  print("Variável Lista PT 02 | Map");
+
+  Map user = {
+    'name': "Kauã Santos",
+    'idade': 17,
+    'altura': 1.80,
+    'Estuda Dart?': true
+  };
+
+  print(user.keys);
+
+  print(user.length);
+
+}
