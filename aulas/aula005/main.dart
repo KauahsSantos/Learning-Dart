4a82f2e6cd4295314103e477a92daf55a9e25012
Kauã Santos <kaua.hs.santos1207@gmail.com>
2025-01-15 18:35:14 -0300

    ceate a coment for explication of the operator mathematical

--- aulas/aula005/main.dart
+++ aulas/aula005/main.dart
@@ -3,7 +3,7 @@ void main(List<String> args) {
   dynamic n2 = 20.1;
   dynamic n3;
 
-  n3 = n1 / n2;
+  n3 = n1 + n2; // or - | * | / for create the simple mathematical operators
 
   print(n3);
 }
