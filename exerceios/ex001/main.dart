0c2a2cc477113f6b94b930a351014460c8225a55
Kauã Santos <kaua.hs.santos1207@gmail.com>
2025-01-15 17:41:46 -0300

    To add exs of the if and else in impor of the data user

---
+++ exerceios/ex001/main.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  print("What's your name?");
+  String? nm = stdin.readLineSync();
+
+  print(nm);
+  print(" é verdade que vc programa em Dart?");
+  String? yn = stdin.readLineSync();
+
+  if (yn == "Sim" || yn == "sim" || yn == "s" || yn == "ss"){
+    print("Esntão é real que vc programe em Dart, a linguage of the Google");
+    print("Very good my friend");
+  }else{
+    print("Blz ent mn");
+  }  
+
+}
