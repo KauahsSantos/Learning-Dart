5b8946d84690b89a7c91c127975d758e800413ac
Kauã Santos <kaua.hs.santos1207@gmail.com>
2025-01-16 18:56:19 -0300

    To add in Map list tips

--- aulas/aula004/main.dart
+++ aulas/aula004/main.dart
@@ -1,15 +1,41 @@
 void main(List<String> args) {
+  dynamic idd = 18;
+  dynamic nm = "Kauã Santos";
+
   print("Variável Lista PT 02 | Map");
 
+  print("");
+
   Map user = {
-    'name': "Kauã Santos",
+    'name': nm,
     'idade': 17,
-    'altura': 1.80,
+    'altura': 1.87,
     'Estuda Dart?': true
   };
 
-  print(user.keys);
+  print(user); // Para deixar a lista mais sucinta usar .keys
+  
+  print(user["Estuda Dart?"]); // Para mostrar os atributos da lista, nesse caso um atributo expecífico.
 
-  print(user.length);
+  print(user.length); // Para mostra a quantidade de dados que a lista tem, ou seja, 1 ou mais.
 
+  print(user);
+
+  user['idade'] = idd;
+  print(user["idade"]);
+
+  print(user);
+
+  print("");
+
+  print("Tipo na variável Map");
+
+   Map<int, dynamic> user2 = {
+    1: "Kauã",
+    2: 17,
+  };
+
+  print("");
+
+  print(user2);
 }
