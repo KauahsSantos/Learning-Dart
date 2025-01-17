d24e24c6450267d4bde7e0ba5589cf5ee4a58fba
Kauã Santos <kaua.hs.santos1207@gmail.com>
2025-01-17 17:07:35 -0300

    To add if and else plus contents

--- aulas/aula007/main.dart
+++ aulas/aula007/main.dart
@@ -1,3 +1,20 @@
+/* > | Maior
+ * < | Menor
+ * >= | Maior igual
+ * <= | Menor igual
+ * != | Diferente
+ * == | Igual á
+*/
+
 void main(List<String> args) {
-  
+  dynamic n1 = 99;
+  dynamic n2 = 100;
+
+  if(n1 >= n2){
+    print("Valor é $n1"); // Para fazermos uma concatenação devemos colocar $ na frente da variável dentro das "".
+  }else if(n1 <= n2){
+    print("$n1 é menor que $n2");
+  }else{
+    print("Erro 404");
+  }
 }
