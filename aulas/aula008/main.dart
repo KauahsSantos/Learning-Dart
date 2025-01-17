33f3f9cc6d575930598c5f402b5317069605c3fa
Kauã Santos <kaua.hs.santos1207@gmail.com>
2025-01-17 17:27:14 -0300

    To add comandas for switch case

--- aulas/aula008/main.dart
+++ aulas/aula008/main.dart
@@ -1,3 +1,21 @@
 void main(List<String> args) {
-  print("Teste");
+
+  print("Iniciando com Switch Case");
+
+  String status = "Andamento";
+
+  switch (status){
+    case "Concluido": 
+      print("Caso concluido");
+      break;
+    case "Incompleto":
+      print("Caso Incompleto");
+      break;
+    case "Andamento":
+      print("Caso em $status");
+      break;
+    default:
+      print("Cara, não tem nenhum status");
+  }
+
 }
