5473ade40fe3903a2f3fe32adc9f6f15db108ec1
Kauã Santos <kaua.hs.santos1207@gmail.com>
2025-01-18 11:05:42 -0300

    To add in class 007 concatenação more accounts

--- aulas/aula007/main.dart
+++ aulas/aula007/main.dart
@@ -13,7 +13,7 @@ void main(List<String> args) {
   if(n1 >= n2){
     print("Valor é $n1"); // Para fazermos uma concatenação devemos colocar $ na frente da variável dentro das "".
   }else if(n1 <= n2){
-    print("$n1 é menor que $n2");
+    print("$n1 é menor que ${n2 + 1 - 1}"); // Podemos tbm fazer contas direto na comcatenação e para isso precisamos colocar, entre a variávem e na frete do $ uma {} - ${var + 1} ou ${var + var1}
   }else{
     print("Erro 404");
   }
