fb580c1f6e0dbc9f12a5f942512d4dff1db78401
Kauã Santos <kaua.hs.santos1207@gmail.com>
2025-01-18 16:08:55 -0300

    Code While

--- aulas/aula010/main.dart
+++ aulas/aula010/main.dart
@@ -1,2 +1,12 @@
 void main(List<String> args) {
+    print("Laços de Repetição While");
+
+    print("");
+
+    dynamic n1 = 1;
+
+    while(n1 >= 10){
+        print("Valor é; $n1");
+        n1++;
+    }
 }
