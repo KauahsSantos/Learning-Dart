068b850bb9ef993647b4528443fb475a76f2c63f
Kauã Santos <kaua.hs.santos1207@gmail.com>
2025-02-11 17:28:24 -0300

    Create a conversor of money

--- exs/dart/basics/ex010/main.dart
+++ exs/dart/basics/ex010/main.dart
@@ -1,3 +1,19 @@
+import 'dart:io';
+
 void main(List<String> args) {
-  
+  print("Conversor de Moedas");
+
+  double vda;
+  double n1, valC;
+
+  print("Digite o valor atual do Dolar");
+  vda = double.parse(stdin.readLineSync()!);
+
+  print("Qual valor você deseja conveter, levendo em consideração a cotação de ${vda}");
+  n1 = double.parse(stdin.readLineSync()!);
+
+  valC = vda * n1;
+
+  print("O valor da Conversão ente ${n1} reais para ${vda} dolares é = ${valC}");
+
 }
