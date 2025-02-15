fe51ec24d1af99c25c99da7ff7a2760523a607b1
Kauã Santos <kaua.hs.santos1207@gmail.com>
2025-02-15 14:16:47 -0300

    Created a exs of fhunction

--- aulas/dart/intermediate/aula001/main.dart
+++ aulas/dart/intermediate/aula001/main.dart
@@ -1,88 +1,33 @@
+import 'dart:io';
+
 void main(List<String> args) {
-  print("Funções em Dart");
-  
-  print("");
-
-  print("Função Sem Parâmetros:");
+  print("Function In Dart");
 
   print("");
 
-  soma();
+  hi();
 
   print("");
 
-  print("Função Com Parãmetros");
-
-  print("");
-
-  nomeIdade("Kauã", 18);
-
-  print("");
-
-  int n2d = 5;
-  print("O dobrdo de $n2d é: " + multiplicacao(n2d).toString());
-
-  print("");
-
-  subtrair(100, 90);
-
-  print("");
-
-  print("Função Simplificada");
-
-  print("");
-
-  print("A multiplicação de 5 * 5 é = " + multiplicar(5, 5).toString());
-
-  print("");
-
-  print(isPar(555 ));
-
-  print("");
-
-  data("Nyck", 22);
-  data("Kauã", 18, number:"") ;
-  data("Kariana", 30, number:"(11 - 8620-09223)");
-  
+  someNunbers();
 }
 
-void soma(){
-  int n1, n2, n3;
-
-  n1 = 10;
-  n2 = 20;
-
-  n3 = n1 + n2;
-
-  print("A soma de $n1 + $n2 é = $n3");
-  
+void hi() {
+  print("Vamos fazer um cálculo");
 }
 
-void nomeIdade(String nm, int idd){
-  print("Meu nome é: $nm e eu tenho $idd anos de idade");
-}
+void someNunbers() {
+  int n1, n2, rs;
 
-int multiplicacao(int n1d){
-  return n1d * 2;
-}
+  print("Digite um número");
+  n1 = int.parse(stdin.readLineSync()!);
 
-void subtrair(int n1b, int n2b){
-  int n3b = n1b - n2b;
-  print("A subtração de $n1b - $n2b é = $n3b");
-}
+  print("");
 
-int multiplicar(int n1c, int n2c) => n1c * n2c;
+  print("Digite outro número");
+  n2 = int.parse(stdin.readLineSync()!);
 
-bool isPar(int val1){
-  if(val1 % 2 == 0){
-    print("$val1 é par");
-    return true;
-  }else{
-    print("Não $val1 não é par");
-    return false;
-  }
-}
+  rs = n1 + n2;
 
-void data(String nome, int idade, {String?  number } ){
-  print(" meu nome é: $nome, eu tenho $idade anos e meu telefone é: $number");
+  print("A some de ${n1} + ${n2} = ${rs}");
 }
