be6caafcf15b417269acedb8bcc8e79366905f19
Kauã Santos <kaua.hs.santos1207@gmail.com>
2025-03-09 20:00:30 -0300

    Create a folder in folder flutter with name -> basics and put the foldes of class in it

--- aulas/dart/intermediate/aula004/main.dart
+++ aulas/dart/intermediate/aula004/main.dart
@@ -1,19 +1,54 @@
 void main(List<String> args) {
-  Saudacoes(
-    10,
-    nome: "Kauã",
-  );
+  Profile(nome: "Kauã Hiago Da Silva Santos", sexo: "Homem", id: 01, idd: 19);
 }
 
-Saudacoes(int id, {
-  String? nome, 
-  String? isDev})
- {
+Profile({
+  required String? nome,  
+  required String? sexo, 
+  required int id, 
+  String? isDev, 
+  int? idd 
+  // required -> Para variáveis/informaçẽos obrigatórias.
+}) { 
+  print("Informações da conta");
 
-  if (nome != null) {
-    print("Seja bem vindo ${nome}");
-  }
-  if (isDev != null) {
-    print("Você é um dev, que legal");
-  }
+  print("");
+
+  print("Nome do Perfil:");
+  String nUser = nome ?? "Nome Não Informado"; // Adiciona a variável nome dentro da nUser se ela não for nula, caso seja deixa como informação, após as ??, "Nome Não Informado". [Isso basicamente é uma simplificação].
+  print(nUser);
+
+  print("");
+
+  print("Sexo");
+  String uSexo = sexo ?? "Não Informado";
+  print(uSexo);
+
+  print("");
+
+  print("ID:");
+  print(id);
+
+  print("");
+
+  print("Idade");
+  dynamic usIdd = idd ?? "Não Informada"; // Diferença pra line 12, transformo a int idd em String
+  print(usIdd);
+
+  print("");
+
+  print("Status:");
+  String sUser = isDev ?? "Não Informado";
+  print("Você é um dev? ${sUser}.");
+
+
+
+  // ?? -> Nome do operador [Nullare] -> que é usado para simplificação de controle de fluxo com variáveis null e noNullskt
+  // -> Faz a mesma coisa que os códigos das linhas 12, 18 e 24.
+  // if (nome != null) {
+  //   print("Seja bem vindo ${nome }");
+  // }
+  // if (isDev != null) {
+  //   print("Você é um dev, que legal");
+  // }
 }
