8eecbed8c0e0ef93d466ea6bc2dfd27bfb7df69c
Kauã Santos <kaua.hs.santos0100@gmail.com>
2025-07-07 15:13:49 -0300

    Removi tudo pra comear do 0 kkakaka

--- aulas/flutter/basics/aula001/android/app/src/main/kotlin/dev/app/aula001/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package dev.app.aula001
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- exs/dart/basics/ex001/main.dart
+++
@@ -1,9 +0,0 @@
-void main(List<String> args) {
-  int a = 25; 
-  int b = 10;
-  int c;
-
-  c = a + b;
-
-  print(c);
-}
