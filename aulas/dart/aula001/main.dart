cea0e0134464ae9c17fe7f202f6390622949e15b
Kauã Santos <kaua.hs.santos0100@gmail.com>
2025-07-07 19:34:58 -0300

    Aula 01 - Tipo de Dados

--- aulas/dart/aula001/main.dart
+++ aulas/dart/aula001/main.dart
@@ -5,24 +5,15 @@ void main(List<String> args) {
 
   print("Aula 01 - Tipo de Dados");
 
-  // int -> para números inteiros
+  int idade = 25;
+  double altura = 1.75;
+  String nome = 'Kauã';
+  bool estudante = true;
+  var linguagem = 'Dart';
+  final curso = 'Curso de Dart';
+  const PI = 3.14159;
+  print('Nome: $nome, Idade: $idade, Altura: $altura, Estudante: $estudante');
+  print('Linguagem: $linguagem, Curso: $curso, PI: $PI');
 
-  int idd = 18;
-  String nm = "Kauã Santo";
-  double alt = 1.72;
-  dynamic eEstudante = true; // Variável do tipo bool
-  var liguagem = "Dart";
-  final curso = "Curso de Dart e Flutter";
-  const nota = 8;
-
-  // Fazendo COnversão da variável booleana pra dynamic para que ao invés de aparecer treu no print aparecer -> Sim
-  dynamic estudante;
-  eEstudante = estudante ?? "Sim";
-
-  print("");
-
-  print(
-    "Meu nome é: $nm tenho $idd anos e $alt de altura, é estudante $eEstudante, \nQual curo você faz? $curso, é um qual linguagem? $liguagem, e sua nota foi? $nota",
-  );
 }
  
