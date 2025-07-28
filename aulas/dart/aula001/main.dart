388a2ab7185c1a2d7abeb42976c0337faae0a52b
Kauã Santos <kaua.hs.santos0100@gmail.com>
2025-07-28 11:46:57 -0300

    Organização, ciei uma pasta para a aula 01

--- aulas/dart/aula001/main.dart
+++ aulas/dart/aula001/main.dart
@@ -1,17 +1,6 @@
 void main(List<String> args) {
+  print("Aula 01 - Primeiro programa em Dart");
   print("Olá Mundo!!");
 
   print("");
-
-  print("Aula 01 - Tipo de Dados");
-
-  int idade = 25;
-  double altura = 1.75;
-  String nome = 'Kauã';
-  bool estudante = true;
-  var linguagem = 'Dart';
-  final curso = 'Curso de Dart';
-  const PI = 3.14159;
-  print('Nome: $nome, Idade: $idade, Altura: $altura, Estudante: $estudante');
-  print('Linguagem: $linguagem, Curso: $curso, PI: $PI');
 }
--- aulas/dart/aula002/main.dart
+++ aulas/dart/aula002/main.dart
@@ -1,29 +1,13 @@
 void main(List<String> args) {
-  print("Aula 02 - Operações Matemáticas");
+  print("Aula 02 - Tipo de Dados");
 
-  int n1;
-  int n2;
-  int r1;
-
-  n1 = 2;
-  n2 = 5;
-
-  // Aqui podemos fazer uma operação de some, caso desejarmos fazer outra opreção basta colocar * -> Multiplicação
-  //                                                                                            / -> Divisão
-  //                                                                                            - -> Subtração
-  r1 = n1 + n2;
-  print(r1);
-
-  // Adicionando números diretamente nas variáveis
-  // n1 = n1 + 10;
-  // print(n1);
-
-  n1 += 10;
-  print(n1);
-
-  // Operações de Resto de Divisão
-  // Para fazermos a divisão de resto de uma divisão basta colocar o sinal da porcentagem -> %
-  // int resto = n1 % n1;
-  // print(resto);
-  print(n1 % n1);
+  int idade = 25;
+  double altura = 1.75;
+  String nome = 'Kauã';
+  bool estudante = true;
+  var linguagem = 'Dart';
+  final curso = 'Curso de Dart';
+  const PI = 3.14159;
+  print('Nome: $nome, Idade: $idade, Altura: $altura, Estudante: $estudante');
+  print('Linguagem: $linguagem, Curso: $curso, PI: $PI');
 }
