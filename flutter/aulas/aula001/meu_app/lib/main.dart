3cafcb46575b12c21b6f65558cb46e6878e94172
Kauã Santos <kaua.hs.santos0100@gmail.com>
2025-08-15 16:46:39 -0300

    Pasta do Flutter - Adicionei imagens em assets/images e inclui essa pasta no /pubspec.yaml - Adicionei ao código as imagens

--- flutter/aulas/aula001/meu_app/lib/main.dart
+++ flutter/aulas/aula001/meu_app/lib/main.dart
@@ -9,7 +9,10 @@ class meu_app extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
+    );
   }
 }
 
@@ -27,93 +30,96 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        // Alinhamento do Eixo Principal -> Y, ou seja vertical, nesse caso o eixo principal do respectivo widget -> Colunm = Coluna
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Entrar",
-            style: TextStyle(
-              color: const Color(0xFF00CCFF),
-              fontWeight: FontWeight.w800,
-              fontSize: 30,
-              letterSpacing: 3,
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image:
+            AssetImage("assets/images/astronaut.png"),
+            fit: BoxFit.cover,
           ),
-
-          const Padding(
-            padding: EdgeInsets.all(40),
-            child: Text(
-              "0",
+        ),
+        child: Column(
+          // Alinhamento do Eixo Principal -> Y, ou seja vertical, nesse caso o eixo principal do respectivo widget -> Colunm = Coluna
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Os flinhos -> Esse é o widget que india ques os que vem a seguir dele são filhos do Column
+            Text(
+              "Entrar",
               style: TextStyle(
-                fontSize: 100,
-                color: Color(0xFF00CCFF),
+                color: const Color(0xFF00CCFF),
                 fontWeight: FontWeight.w800,
+                fontSize: 30,
+                letterSpacing: 3,
               ),
-            ), 
-          ),
-          // SizedBox(
-          //   width: 5,
-          // ),
-          Row(
-            // Alinhamento do Eixo Secundário -> X, ou seja horizontal, nesse caso o eixo secundário do respectivo widget -> Row = Linha
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [ 
-              TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                  backgroundColor: const Color(0xFF00CCFF),
-                  fixedSize: Size(125, 125),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: Colors.black, width: 2.5,
-                    ),
-                    borderRadius: BorderRadiusGeometry.circular(15),
-                  ),
-                  // padding: const EdgeInsets.all(30),
-                ),
-                child: Text(
-                  "Saiu",
-                  style: TextStyle(
-                    color: const Color(0xFFFFFFFF),
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
-                    letterSpacing: 2,
-                  ),
-                ),
-              ),
+            ),
 
-              SizedBox(
-                width: 30,
+            const Padding(
+              padding: EdgeInsets.all(40),
+              child: Text(
+                "0",
+                style: TextStyle(
+                  fontSize: 100,
+                  color: Color(0xFF00CCFF),
+                  fontWeight: FontWeight.w800,
+                ),
               ),
-              
-              TextButton(
-                onPressed: increment,
-                style: TextButton.styleFrom(
-                  backgroundColor: const Color(0xFF00CCFF),
-                  fixedSize: Size(125, 125),
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: Colors.black, width: 2.5,
+            ),
+            // SizedBox(
+            //   width: 5,
+            // ),
+            Row(
+              // Alinhamento do Eixo Secundário -> X, ou seja horizontal, nesse caso o eixo secundário do respectivo widget -> Row = Linha
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: decrement,
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color(0xFF00CCFF),
+                    fixedSize: Size(125, 125),
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.black, width: 2.5),
+                      borderRadius: BorderRadiusGeometry.circular(15),
                     ),
-                    borderRadius: BorderRadiusGeometry.circular(15)
+                    // padding: const EdgeInsets.all(30),
                   ),
-                  // padding: EdgeInsets.all(30),
-                ),
-                child: Text(
-                  "Entrou",
-                  style: TextStyle(
-                    color: const Color(0xFFFFFFFF),
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
-                    letterSpacing: 2,
+                  child: Text(
+                    "Saiu",
+                    style: TextStyle(
+                      color: const Color(0xFFFFFFFF),
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 2,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+
+                SizedBox(width: 30),
+
+                TextButton(
+                  onPressed: increment,
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color(0xFF00CCFF),
+                    fixedSize: Size(125, 125),
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.black, width: 2.5),
+                      borderRadius: BorderRadiusGeometry.circular(15),
+                    ),
+                    // padding: EdgeInsets.all(30),
+                  ),
+                  child: Text(
+                    "Entrou",
+                    style: TextStyle(
+                      color: const Color(0xFFFFFFFF),
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
-  } 
+  }
 }
