04813cd758713322a9e128262ef7e7bae57c2a98
Kauã Santos <kaua.hs.santos0100@gmail.com>
2026-05-22 21:52:17 -0300

    to add aula02

---
+++ pratic/flutter/aulas/aula002/app_container/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    title: "Projeto Container",
+    home: Container(    
+        color: Colors.amber,
+    ),
+  ));
+}
