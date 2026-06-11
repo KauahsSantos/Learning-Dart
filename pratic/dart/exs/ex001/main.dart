0779f623b1aa94fbed10c699d4a7ec63c8a9eb52
Kauã Santos <kaua.hs.santos0100@gmail.com>
2026-06-10 22:23:34 -0300

    To add /aula003 and folders of the exs

---
+++ pratic/dart/exs/ex001/main.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  print("Crie uma variável para armazenar seu nome e idade");
+
+  print("");
+
+  String nm = "Kauã";
+  int idd = 18;
+
+  print("Meu nome é $nm e tenho $idd anos");
+}
---
+++ pratic/dart/exs/ex002/main.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  print("Crie uma lista com 3 tecnologias que você quer aprender");
+
+  List<String> tec = ["Dart", "Flutter", "Java", "Swift"];
+
+  print("As tecnologias que eu quero aprender são: $tec");
+}
