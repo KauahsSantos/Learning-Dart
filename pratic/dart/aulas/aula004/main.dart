c237beb42f1db3c210b0cda957dcd6e44499b7f0
Kauã Santos <kaua.hs.santos0100@gmail.com>
2026-07-03 23:08:49 -0300

    Star codes of the Flux Contronl and more

--- pratic/dart/aulas/aula004/main.dart
+++ pratic/dart/aulas/aula004/main.dart
@@ -1,3 +1,19 @@
 void main(List<String> args) {
-  print("");
+  print("Introdução a Estruturas Condicionais");
+
+  int n1 = 15;
+  int slr = 0;
+
+  if (n1 <= 17 && slr == 0) {
+    print("É menor de idade");
+    print("Não tem salário");
+  } else if (n1 >= 18 && slr <= 1650) {
+    print("É maior de idade");
+    print("Recebe o salário minimo");
+  } else if (n1 >= 18 && slr >= 1800) {
+    print("É maior de idade");
+    print("Recebe mais que o salário mínimo");
+  } else if (n1 >= 18 || slr > 7000) {
+    print("Não sei sua idade mais tu recebe bem kkkk");
+  }
 }
