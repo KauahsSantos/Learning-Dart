9c736141eca3a11cee1d2f1175d468f3833e5ae2
Kauã Santos <kaua.hs.santos0100@gmail.com>
2026-09-07 14:02:02 -0300

    Update in exs 14, more forms of the input of date

--- pratic/dart/exs/ex014/main.dart
+++ pratic/dart/exs/ex014/main.dart
@@ -4,9 +4,31 @@ void main(List<String> args) {
   print("Recebendo Dados do Usuario!\n");
 
   String nm;
+  int idd;
+  double? slr;
+  bool? investe;
 
   print("Digite seu nome:");
   nm = stdin.readLineSync()!;
 
   print("\nOla, seu nome e: $nm");
+
+  print("\nDigite tua idade:");
+  idd = int.parse(stdin.readLineSync()!);
+
+  print("Sua idade e: $idd");
+
+  print("\nQual seu Salario:");
+  slr = double.tryParse(stdin.readLineSync()!);
+
+  print("Seu salario e: $slr");
+
+  print("\nVoce investe em alguma coisa:");
+  investe = bool.tryParse(stdin.readLineSync()!);
+
+  if (investe == true) {
+    print("sim, eu invisto");
+  } else {
+    print("Nao");
+  }
 }
