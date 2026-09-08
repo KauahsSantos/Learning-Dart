704cea4201ea5d19ed13995b7b782fddaef24632
Kauã Santos <kaua.hs.santos0100@gmail.com>
2026-09-08 18:42:16 -0300

    Add aula05.dart

---
+++ pratic/dart/aulas/aula006/aula05.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  print("Introdução a Função - Aula 04 - Função Nullables");
+
+  String nm = addNomes();
+  print(nm);
+
+  String ola = olaMundo(11) ?? "Função Nula";
+  print(ola.toUpperCase());
+}
+
+String addNomes() => "\nKaua".toUpperCase();
+
+String? olaMundo(int n1) {
+  if (n1 <= 10) {
+    return "Ola Mundo";
+  }
+}
