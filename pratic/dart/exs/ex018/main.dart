8adfd939ce61b85f05d8b4d435df5a995d574c88
Kauã Santos <kaua.hs.santos0100@gmail.com>
2026-09-09 17:14:15 -0300

    Adicionado Ex018

---
+++ pratic/dart/exs/ex018/main.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  print("Ex 01 - Percorrendo uma lista\n");
+
+  List<int> numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  for (int i = 0; i < numeros.length; i++) {
+    print("Posição $i : ${numeros[i]}");
+  }
+}
