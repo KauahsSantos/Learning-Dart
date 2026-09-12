51ed9a5aaa9b438429f46038a85180bc45e8f828
Kauã Santos <kaua.hs.santos0100@gmail.com>
2026-09-12 11:12:22 -0300

    Ex027

---
+++ pratic/dart/exs/ex027/main.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  print("Aplicando Imporsto");
+
+  var n1;
+  List<int> valores = [1200, 2000, 3200, 1621, 2100];
+
+  print("A tabela de salaros e: $valores\n");
+
+  print("Escolha quanto % sera aplicado de imposto em cada salario:");
+  n1 = int.parse(stdin.readLineSync()!);
+
+  var valoresImposto = valores.map((pos) => pos + n1*12);
+
+  print(
+    "A tabela dos salarios com mais $n1% de imposto adiconado fica: $valoresImposto",
+  );
+
+  var valoresDescontado = valores.map((pos) => pos - n1);
+
+  print(
+    "\nA tabela dos salarios com menos $n1% de imposto fica: $valoresDescontado",
+  );
+}
