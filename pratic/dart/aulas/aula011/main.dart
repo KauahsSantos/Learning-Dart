a940d17f471694c84943aef943c853082ff380b4
Kauã Santos <kaua.hs.santos0100@gmail.com>
2026-09-19 17:48:59 -0300

    Aula 011 - Update in Json -  inport of the file for main.dart

--- pratic/dart/aulas/aula011/main.dart
+++ pratic/dart/aulas/aula011/main.dart
@@ -1,4 +1,12 @@
-void main(List<String> args) {
+import 'dart:convert';
+import 'dart:io';
+
+void main(List<String> args) async {
   print("Introdução a JSON");
-  
+
+  final arquivo = File("main.json");
+  final conteudo = await arquivo.readAsString();
+  final dados = jsonDecode(conteudo);
+
+  print(dados["linguagens"][1]["Python"]);
 }
