e1e2608ae98f55164e27850c09ab305c6e436b88
Kauã Santos <kaua.hs.santos0100@gmail.com>
2026-09-20 17:16:02 -0300

    Move exs for new folders

--- pratic/dart/exs/ex015/main.dart
+++
@@ -1,7 +0,0 @@
-void main(List<String> args) {
-  print("Crie uma lista com 3 tecnologias que você quer aprender");
-
-  List<String> tec = ["Dart", "Flutter", "Java", "Swift"];
-
-  print("As tecnologias que eu quero aprender são: $tec");
-}
--- pratic/dart/exs/ex018/main.dart
+++
@@ -1,9 +0,0 @@
-void main(List<String> args) {
-  print("Percorrendo uma lista\n");
-
-  List<int> numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  for (int i = 0; i < numeros.length; i++) {
-    print("Posição $i : ${numeros[i]}");
-  }
-}
