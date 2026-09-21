1f681ab7ea172cdf76effd96f433869343f58e5c
Kauã Santos <kaua.hs.santos0100@gmail.com>
2026-09-21 16:27:05 -0300

    aula13

---
+++ pratic/dart/aulas/aula013/main.dart
@@ -0,0 +1,16 @@
+enum statusPagamento { pago, pendente, extorno }
+
+void main(List<String> args) {
+  print("Introducao a Enuns");
+
+  statusPagamento status = statusPagamento.pendente;
+
+  switch (status) {
+    case statusPagamento.pago:
+      print("Pagamento efetuado");
+    case statusPagamento.pendente:
+      print("Pagamento pendente");
+    case statusPagamento.extorno:
+      print("Valor extornado");
+  }
+}
