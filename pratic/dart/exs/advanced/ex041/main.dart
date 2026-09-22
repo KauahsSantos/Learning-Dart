4a3ecacd1945c6d57867e785080383d2e268a4b6
Kauã Santos <kaua.hs.santos0100@gmail.com>
2026-09-22 16:08:25 -0300

    Ex041

--- pratic/dart/exs/advanced/ex041/main.dart
+++ pratic/dart/exs/advanced/ex041/main.dart
@@ -1,3 +1,18 @@
+enum statusPedido { enviado, entregue, pendente, cancelado, processando }
+
 void main(List<String> args) {
-  
+  statusPedido pedido = statusPedido.pendente;
+
+  switch (pedido) {
+    case statusPedido.enviado:
+      print("Seu pedido foi ${pedido.name}");
+    case statusPedido.entregue:
+      print("Seu pedido foi ${pedido.name}");
+    case statusPedido.pendente:
+      print("Seu pedido esta ${pedido.name}");
+    case statusPedido.cancelado:
+        print("Seu pedido foi ${pedido.name}");
+    case statusPedido.processando:
+      print("Seu pedido esta sendo ${pedido.name}");
+  }
 }
